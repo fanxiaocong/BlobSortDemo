0f4f79dd67ac438573afc81fb5377c8ef562de0e
@fanxiaocong <1016697223@qq.com>
2021-01-05 17:58:43 +0800

    修改演示案例

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
 const Sort = require('./sort')
 
+
+/*********************** 数组的默认排序 ***********************/ 
+/*
+let array1 = ['banana', 'cherry', 'apple']
+array1.sort()  // [ 'apple', 'banana', 'cherry' ]
+ 
+let array2 = [33, 4, 1111, 222]
+array2.sort()  // [ 1111, 222, 33, 4 ]
+ 
+let array3 = [NaN, 10, undefined, null, 'Nc']
+array3.sort()  // [ 10, NaN, 'Nc', null, undefined ]
+*/
+/*********************** 数组的默认排序 ***********************/ 
+
+
+
 const person = (first, last, age) => ({ first, last, age })
 
 let people = [
@@ -11,7 +27,30 @@ let people = [
   person('Andy', 'Brown', 5)
 ]
 
+/*********************** 使用比较函数进行排序 ***********************/ 
+/*
+people.sort((p0, p1) => {
+  if (p0.last > p1.last)  return 1
+  if (p0.last < p1.last)  return -1
+  return 0
+})
 
+people.sort((p0, p1) => {
+  // 按照 last 升序
+  if (p0.last > p1.last)  return 1
+  if (p0.last < p1.last)  return -1
+
+  // 如果 last 相同，再按照 first 升序
+  if (p0.first > p1.first)  return 1
+  if (p0.first < p1.first)  return -1
+
+  return 0
+})
+*/
+/*********************** 使用比较函数进行排序 ***********************/ 
+
+
+/*********************** 使用排序描述符进行排序 ***********************/ 
 // 按照 last 升序
 const sortByLast = Sort.descriptor(item => item.last)
 // 按照 first 升序
@@ -26,3 +65,4 @@ console.log(people)
 
 let arr = [1, 7, 9, 3]
 console.log(arr.sort(Sort.descriptor()))
+/*********************** 使用排序描述符进行排序 ***********************/ 
